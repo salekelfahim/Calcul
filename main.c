4d15b35c43750febf16bb55b5bb4bc25376e44d0
salekelfahim <salekelfahim@gmail.com>
2023-09-14 14:50:42 +0100

    Calcul using switch

--- main.c
+++ main.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 
-int main (){
+int main(){
+
+
 float n1;
 float n2;
 int op;
-
+float reslt;
 printf("Give me a number:");
 scanf("%f", &n1);
 printf("Give me another number:");
 scanf("%f", &n2);
 printf("Choose the operator: 1-Multiplication   2-Addition   3-Subtraction   4-Division  ");
 scanf("%d", &op);
-if (op== 1 ) {
-float reslt = n1 * n2;
+
+switch(op) {
+
+case (1) :
+    reslt = n1 * n2;
     printf("The result is %.2f", reslt);
+break;
 
-}
-
-if (op==2){
-    float reslt = n1 + n2;
-    printf("THe result is %.2f", reslt);
-}
-if (op==3){
-    float reslt = n1 - n2;
+case (2) :
+    reslt = n1 + n2;
     printf("The result is %.2f", reslt);
-}
-if (op==4){
-    float reslt = n1 / n2;
-    printf ("The result is %.2f", reslt);
+break;
+
+case (3) :
+    reslt = n1 - n2;
+    printf("The result is %.2f", reslt);
+break;
+
+case (4) :
+    reslt = n1 / n2;
+    printf("The result is %.2f", reslt);
+break;
 
 }
+
 
 return 0;
+
 }
